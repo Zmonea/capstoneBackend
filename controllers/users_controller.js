483d72113429db46ce95d4e057e7a3246273f285
zmonea <Zac.j.monea@gmail.com>
2021-12-06 14:40:08 -0500

    Backend Login working

---
+++ controllers/users_controller.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt')
+const express = require('express')
+const user = express.Router()
+const User = require('../models/users.js')
+
+
+user.get('/', (req, res) => {
+  res.json('Hello World')
+})
+
+user.post('/createaccount', (req, res) => {
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+  console.log(req.body);
+  User.create(req.body, (err, createdUser) => {
+    if(err){
+      console.log(err);
+      res.json(err.message)
+    } else {
+      console.log('user is created', createdUser);
+      res.json(createdUser)
+    }
+  })
+});
+
+
+user.put('/login', (req, res) => {
+  console.log(req.body);
+  User.findOne({username: req.body.username}, (err, foundUser) => {
+    if(err) {
+      res.json('Sorry, but the Database ran into a problem. Please try again.')
+    } else {
+      if(!foundUser){
+        res.json('This user was not found. Please try again.')
+      } else if(bcrypt.compareSync(req.body.password, foundUser.password)) {
+        res.json({username: foundUser.username})
+      } else {
+        res.json('Password does not match. Please try again.')
+      }
+    }
+  })
+});
+
+
+module.exports = user
---
+++ server.js
@@ -0,0 +1,81 @@
+//Dependencies
+const express = require('express')
+const mongoose = require('mongoose')
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const socket = require('socket.io')
+const app = express()
+const server = require("http").createServer(app);
+let io = require('socket.io')(server, {cors: { origin: "*" }})
+
+const cors = require('cors')
+
+const db = mongoose.connection
+require('dotenv').config()
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+
+// Port
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3003
+
+// Database
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI)
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
+//Controllers
+
+// const carsController = require('./controllers/cars.js')
+// const userController = require('./controllers/users_controller.js')
+// app.use('/cars', carsController)
+// app.use('/', userController)
+
+
+
+app.get('/', (req,res) => {
+   res.send('Hello World')
+})
+
+//Listener
+server.listen(PORT, () => {
+  console.log('Server is Listening...', PORT);
+})
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to Mongod...');
+})
+//Socket IO setup
+
+io = socket(server)
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+  socket.on('join_room', (data) => {
+    socket.join(data)
+    console.log(`User Joined Room:` + data);
+  })
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on('disconnect', (socket) => {
+    console.log('USER DISCONNECTED');
+  })
+})
