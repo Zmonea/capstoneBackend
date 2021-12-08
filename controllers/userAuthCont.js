3f6a99d4a241bf020c48e6adb3f4b54910ce98a9
zmonea <Zac.j.monea@gmail.com>
2021-12-08 12:18:01 -0500

    Trouble shooting heroku issues with tokens

--- controllers/userAuthCont.js
+++ controllers/userAuthCont.js
@@ -2,13 +2,11 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/users");
 const generateToken = require("../config/generateToken");
 
-//@description     Get or Search all users
-//@route           GET /api/user?search=
-//@access          Public
+
 const allUsers = asyncHandler(async (req, res) => {
   const keyword = req.query.search
     ? {
-        //Mondo DB Regex Option to find Name and Email without case sensitivity
+        
         $or: [
           { name: { $regex: req.query.search, $options: "i" } },
           { email: { $regex: req.query.search, $options: "i" } },
@@ -20,9 +18,7 @@ const allUsers = asyncHandler(async (req, res) => {
   res.send(users);
 });
 
-//@description     Register new user
-//@route           POST /api/user/
-//@access          Public
+
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
 
@@ -60,9 +56,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-//@description     Auth the user
-//@route           POST /api/users/login
-//@access          Public
+
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
